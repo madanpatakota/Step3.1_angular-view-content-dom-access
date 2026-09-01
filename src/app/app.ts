18d3abca57f2e23218a8df6d5a9950a4b98cd0c3
Madan.patakota <madan.patakota@gmail.com>
2026-09-01 16:29:40 +0530

    view-content-basics Covered

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,12 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from '@angular/core';
+import { Employee } from './employee/employee';
 
 @Component({
-  imports: [RouterOutlet],
+  imports: [Employee],
   selector: 'app-root',
   styleUrl: './app.css',
   templateUrl: './app.html',
 })
 export class App {
-  protected readonly title = signal('Step3.1_angular-view-content-dom-access');
+  
 }
