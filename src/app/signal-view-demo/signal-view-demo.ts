a6437d305539db79ffccaa05fed1aa1d302d0ea6
Madan.patakota <madan.patakota@gmail.com>
2026-09-04 20:13:52 +0530

    done

--- src/app/signal-view-demo/signal-view-demo.ts
+++ src/app/signal-view-demo/signal-view-demo.ts
@@ -1,50 +1,60 @@
 import {
   Component,
+  computed,
   ElementRef,
+  signal,
   viewChild,
-  viewChildren
+  viewChildren,
 } from '@angular/core';
 
 @Component({
   selector: 'app-signal-view-demo',
   imports: [],
   templateUrl: './signal-view-demo.html',
-  styleUrl: './signal-view-demo.css'
+  styleUrl: './signal-view-demo.css',
 })
 export class SignalViewDemoComponent {
+  // Example 1: Optional signal query
+  employeeName = viewChild<ElementRef<HTMLInputElement>>('employeeName');
 
-  // Example 1
-  employeeName = viewChild<ElementRef>('employeeName');
-
-  // Example 2
+  // Example 2: Required signal query
   employeeEmail =
-    viewChild.required<ElementRef>('employeeEmail');
+    viewChild.required<ElementRef<HTMLInputElement>>('employeeEmail');
 
-  // Example 3
-  employees =
-    viewChildren<ElementRef>('employee');
+  // Example 3: Multiple elements as a signal
+  employees = viewChildren<ElementRef<HTMLInputElement>>('employee');
 
+  // Signals used to show result in the UI
+  selectedEmployeeName = signal('');
+  selectedEmployeeEmail = signal('');
+  employeeList = signal<string[]>([]);
+
+  // This value updates automatically when employee elements are added/removed
+  employeeCount = computed(() => this.employees().length);
+
+  showMoreEmployees = signal(false);
 
   showEmployee() {
-    console.log(
-      this.employeeName()?.nativeElement.value
-    );
-  }
+    const name = this.employeeName()?.nativeElement.value.trim() ?? '';
 
+    this.selectedEmployeeName.set(name);
+  }
 
   showEmail() {
-    console.log(
-      this.employeeEmail().nativeElement.value
-    );
-  }
+    const email = this.employeeEmail().nativeElement.value.trim();
 
+    this.selectedEmployeeEmail.set(email);
+  }
 
   showEmployees() {
+    const employeeNames = this.employees()
+      .map((employee) => employee.nativeElement.value.trim())
+      .filter((name) => name !== '');
 
-    this.employees().forEach(employee => {
-      console.log(employee.nativeElement.value);
-    });
-
+    this.employeeList.set(employeeNames);
   }
 
+  addMoreEmployees() {
+    this.showMoreEmployees.set(true);
+  }
 }
